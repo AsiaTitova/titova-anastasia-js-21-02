6c9d364947743f3665ae9106bf13dd29478a166e
asiatitova <kutsanastasia@yandex.ru>
2021-10-25 19:26:03 +0700

    homework 07, class, oop

---
+++ homework_07_class_OOP/objectClass.js
@@ -0,0 +1,192 @@
+
+// Выполнить то же самое, используя классы
+
+class AnimalClass {
+  constructor(name) {
+    this._name = name;
+  }
+
+  eat() {
+    console.log(`${this._name} ест`);
+  }
+
+  say = function() {
+    console.log(`Неизвестное животное молчит`);
+  }
+
+  set name(name) {
+    this._name = name;
+    console.log(`Мне поменяли имя на ${name}`)
+  }
+}
+
+Object.defineProperties(AnimalClass, {
+  'name': {
+    configurable: false,
+    writable: false,
+    enumerable: true
+  },
+  'say': {
+    configurable: false,
+    writable: false,
+    enumerable: false
+  },
+  'eat': {
+    configurable: false,
+    writable: false,
+    enumerable: false
+  }
+});
+
+// console.log(Object.keys(AnimalClass))
+// console.log(Object.getOwnPropertyDescriptors(AnimalClass))
+
+class CatClass extends AnimalClass {
+  constructor(name, type) {
+    super(name);
+    this._name = name;
+    this.type = type;
+  }
+
+  hunt() {
+    console.log(`${this._name} охотится`)
+  }
+
+  say = function() {
+    console.log(`${this.type} мяукает`);
+  }
+}
+
+Object.defineProperties(CatClass, {
+  'name': {
+    configurable: false,
+    writable: false,
+    enumerable: true
+  },
+  'type': {
+    configurable: false,
+    writable: false,
+    enumerable: true
+  },
+  'say': {
+    configurable: false,
+    writable: false,
+    enumerable: false
+  },
+  'hunt': {
+    configurable: false,
+    writable: false,
+    enumerable: false
+  },
+  '__proto__': {
+    configurable: false,
+    writable: false,
+    enumerable: false
+  }
+});
+
+// console.log(Object.keys(CatClass))
+// console.log(Object.getOwnPropertyDescriptors(CatClass))
+
+class DogClass extends AnimalClass {
+  constructor(name, type) {
+    super(name);
+    this._name = name;
+    this.type = type;
+  }
+
+  say = function() {
+    console.log(`${this.type} мяукает`);
+  }
+}
+
+Object.defineProperties(DogClass, {
+  'name': {
+    configurable: false,
+    writable: false,
+    enumerable: true
+  },
+  'type': {
+    configurable: false,
+    writable: false,
+    enumerable: true
+  },
+  'say': {
+    configurable: false,
+    writable: false,
+    enumerable: false
+  },
+  'hunt': {
+    configurable: false,
+    writable: false,
+    enumerable: false
+  },
+  '__proto__': {
+    configurable: false,
+    writable: false,
+    enumerable: false
+  }
+});
+
+// console.log(Object.keys(DogClass))
+// console.log(Object.getOwnPropertyDescriptors(DogClass))
+
+class ParrotClass extends AnimalClass  {
+  constructor(name, type) {
+    super(name);
+    this._name = name;
+    this.type = type;
+  }
+
+  say = function() {
+    console.log(`${this.type} говорит`);
+  }
+}
+
+Object.defineProperties(ParrotClass, {
+  'name': {
+    configurable: false,
+    writable: false,
+    enumerable: true
+  },
+  'type': {
+    configurable: false,
+    writable: false,
+    enumerable: true
+  },
+  'say': {
+    configurable: false,
+    writable: false,
+    enumerable: false
+  },
+  'hunt': {
+    configurable: false,
+    writable: false,
+    enumerable: false
+  },
+  '__proto__': {
+    configurable: false,
+    writable: false,
+    enumerable: false
+  }
+});
+
+// console.log(Object.keys(ParrotClass))
+// console.log(Object.getOwnPropertyDescriptors(ParrotClass))
+
+
+const ANIMAL_CLASS = new AnimalClass('Andreas');
+const CAT_CLASS = new CatClass('Китти', 'Кошка');
+const DOG_CLASS = new DogClass('Бобик', 'Собака');
+const PARROT_CLASS = new ParrotClass('Кеша', 'Попугай');
+
+
+ANIMAL_CLASS.eat();
+ANIMAL_CLASS.say();
+CAT_CLASS.eat();
+CAT_CLASS.say();
+CAT_CLASS.hunt();
+DOG_CLASS.eat();
+DOG_CLASS.say();
+PARROT_CLASS.eat();
+PARROT_CLASS.say();
---
+++ homework_07_class_OOP/objectFunction.js
@@ -0,0 +1,180 @@
+'use strict'
+
+//Выполнить то же самое использую функции конструкторы.
+
+function AnimalFunc(name) {
+  this.name = name;
+
+  this.eat = function() {
+    console.log(`${this.name} ест`);
+  };
+
+  this.say = function() {
+    console.log(`Неизвестное животное молчит`);
+  }
+}
+
+Object.defineProperties(AnimalFunc, {
+  'name': {
+    configurable: false,
+    writable: false,
+    enumerable: true
+  },
+  'say': {
+    configurable: false,
+    writable: false,
+    enumerable: false
+  },
+  'eat': {
+    configurable: false,
+    writable: false,
+    enumerable: false
+  }
+});
+
+// console.log(Object.keys(AnimalFunc))
+// console.log(Object.getOwnPropertyDescriptors(AnimalFunc))
+
+function CatFunc(name, type) {
+  AnimalFunc.call(this, name);
+  this.name = name;
+  this.type = type;
+
+  this.hunt = function() {
+    console.log(`${this.name} охотится`)
+  }
+
+  let parentSay = this.say;
+  this.say = function() {
+    if (this.type) {
+      console.log(`${this.type} мяукает`);
+    } else {
+      parentSay();
+    }
+  }
+}
+
+
+Object.defineProperties(CatFunc, {
+  'name': {
+    configurable: false,
+    writable: false,
+    enumerable: true
+  },
+  'type': {
+    configurable: false,
+    writable: false,
+    enumerable: true
+  },
+  'say': {
+    configurable: false,
+    writable: false,
+    enumerable: false
+  },
+  'hunt': {
+    configurable: false,
+    writable: false,
+    enumerable: false
+  },
+  '__proto__': {
+    configurable: false,
+    writable: false,
+    enumerable: false
+  }
+});
+
+// console.log(Object.keys(CatFunc))
+// console.log(Object.getOwnPropertyDescriptors(CatFunc))
+
+
+function DogFunc(name, type) {
+  AnimalFunc.call(this, name);
+  this.name = name;
+  this.type = type;
+
+  let parentSay = this.say;
+  this.say = function() {
+    if (this.type) {
+      console.log(`${this.type} лает`);
+    } else {
+      parentSay();
+    }
+  }
+}
+
+
+Object.defineProperties(DogFunc, {
+  'name': {
+    configurable: false,
+    writable: false,
+    enumerable: true
+  },
+  'type': {
+    configurable: false,
+    writable: false,
+    enumerable: true
+  },
+  'say': {
+    configurable: false,
+    writable: false,
+    enumerable: false
+  },
+  '__proto__': {
+    configurable: false,
+    writable: false,
+    enumerable: false
+  }
+});
+
+function ParrotFunc(name, type) {
+  AnimalFunc.call(this, name);
+  this.name = name;
+  this.type = type;
+
+  let parentSay = this.say;
+  this.say = function() {
+    if (this.type) {
+      console.log(`${this.type} говорит`);
+    } else {
+      parentSay();
+    }
+  }
+}
+
+Object.defineProperties(ParrotFunc, {
+  'name': {
+    configurable: false,
+    writable: false,
+    enumerable: true
+  },
+  'type': {
+    configurable: false,
+    writable: false,
+    enumerable: true
+  },
+  'say': {
+    configurable: false,
+    writable: false,
+    enumerable: false
+  },
+  '__proto__': {
+    configurable: false,
+    writable: false,
+    enumerable: false
+  }
+});
+
+const ANIMAL_FUNC = new AnimalFunc('Andreas');
+const CAT_FUNC = new CatFunc('Китти', 'Кошка');
+const DOG_FUNC = new DogFunc('Бобик', 'Собака');
+const PARROT_FUNC = new ParrotFunc('Кеша', 'Попугай');
+
+// ANIMAL_FUNC.eat();
+// ANIMAL_FUNC.say();
+// CAT_FUNC.eat();
+// CAT_FUNC.say();
+// CAT_FUNC.hunt();
+// DOG_FUNC.eat();
+// DOG_FUNC.say();
+// PARROT_FUNC.eat();
+// PARROT_FUNC.say();
