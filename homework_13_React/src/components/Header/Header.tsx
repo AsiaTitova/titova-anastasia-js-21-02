c58e393bb9c7358c875e5752d846318e42d3ac24
asiatitova <kutsanastasia@yandex.ru>
2021-11-20 13:22:36 +0700

    react page

--- homework_13_React/src/components/Header/Header.tsx
+++ homework_13_React/src/components/Header/Header.tsx
@@ -7,10 +7,6 @@ export class Header extends React.Component {
       <header className="header">
         <div className="header__container container">
           <h1 className="header__logo">Интернет-магазин "Не только красивое"</h1>
-          <div className="header__switch switch">
-            <input className="switch__checkbox visually-hidden" type="checkbox" id="dark-theme" name="dark-theme" />
-            <label className="switch__label" htmlFor="dark-theme" />
-          </div>
         </div>
       </header>
     )
